a5315fb6a031278829f57f9c0041edef77e81ba8
Lyon-Home <honpan.loi@gmail.com>
2021-02-22 20:38:18 -0600

    saving progress

---
+++ ExpenseReimbursementSystem/src/main/java/com/revature/res/controller/RequestHelper.java
@@ -0,0 +1,54 @@
+package com.revature.res.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class RequestHelper {
+	
+	public static Object processGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		final String RESOURCE = getURI(request);
+		
+		switch (RESOURCE) {
+		case "/login":
+			
+			return "I'm logging in";
+
+		default:
+			response.setStatus(404);
+			return "Sorry. The resource you have requested does not exist.";
+		}
+		
+	}
+
+	private static String getURI(HttpServletRequest request) {
+		final String URI = request.getRequestURI();
+		System.out.println(URI);
+		final String RESOURCE = URI.replace("/ExpenseReimbursementSystem", "");
+		System.out.println(RESOURCE);
+		return RESOURCE;
+	}
+	
+	public static void processPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		final String RESOURCE = getURI(request);
+		
+		switch (RESOURCE) {
+		case "/api/login":
+			final String email = request.getParameter("useremail");
+			final String password = request.getParameter("userpass");
+			
+			System.out.println(email);
+			System.out.println(password);
+			
+			break;
+
+		default:
+			response.setStatus(404);
+			response.getWriter().write("Sorry. The resource you have requested does not exist.");
+			break;
+		}
+	}
+}
