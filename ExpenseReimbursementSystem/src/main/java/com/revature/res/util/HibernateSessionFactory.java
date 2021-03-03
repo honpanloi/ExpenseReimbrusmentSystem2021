19c1fe6d4ed98b82f96e3fc9c7a7aedd7346e3af
Lyon-Home <honpan.loi@gmail.com>
2021-03-02 21:14:37 -0600

    update environment variable

--- ExpenseReimbursementSystem/src/main/java/com/revature/res/util/HibernateSessionFactory.java
+++ ExpenseReimbursementSystem/src/main/java/com/revature/res/util/HibernateSessionFactory.java
@@ -12,9 +12,9 @@ public class HibernateSessionFactory {
 		
 		if(sessionFactory==null) {
 			sessionFactory = new Configuration().configure()
-					.setProperty("hibernate.connection.url", System.getenv("dburl"))
-					.setProperty("hibernate.connection.username", System.getenv("dbUserName"))
-					.setProperty("hibernate.connection.password", System.getenv("dbPassword"))
+					.setProperty("hibernate.connection.url", System.getenv("DBURL"))
+					.setProperty("hibernate.connection.username", System.getenv("DBUSERNAME"))
+					.setProperty("hibernate.connection.password", System.getenv("DBPASSWORD"))
 					.buildSessionFactory();
 		}
 		return sessionFactory.getCurrentSession();
